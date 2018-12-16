47796790e4c1062cf3cdc5fffeec1b15e6909a2f
Jon Alberdi <j.alberdi@criteo.com>
2018-12-16 15:59:44 +0100

    Add test for to verify bids with IncrementSlots

--- src/test/scala/com/ion/trials/akka/auction/OpennedSpec.scala
+++ src/test/scala/com/ion/trials/akka/auction/OpennedSpec.scala
@@ -1,7 +1,13 @@
 package com.ion.trials.akka.auction
 
 import akka.http.scaladsl.model.StatusCodes
-import com.ion.trials.akka.auction.AuctionTypes.{Answer, Bid}
+import com.ion.trials.akka.auction.AuctionTypes.{
+  Answer,
+  Bid,
+  Increment,
+  IncrementSlot,
+  Price
+}
 import com.ion.trials.akka.auction.Openned.{NewBid, NewBidder}
 import com.ion.trials.akka.util.AuctionTestData
 import org.scalacheck.Properties
@@ -12,10 +18,16 @@ object OpennedSpec
     extends Properties("Openned auction tests")
     with AuctionTestData {
 
+  val firstSlot = IncrementSlot(endOfSlot = 10, minIncrement = 2)
+  val lastSlot = IncrementSlot(endOfSlot = Integer.MAX_VALUE, minIncrement = 10)
+
   val openned = new Openned(
-    new Planned(rule = auctionRule.copy(initialPrice = 10),
-                auctionId = 0,
-                auctioneerId = 1))
+    new Planned(
+      rule = auctionRule.copy(initialPrice = 10,
+                              increment = Increment(List(firstSlot, lastSlot))),
+      auctionId = 0,
+      auctioneerId = 1
+    ))
 
   val joinedBidders = List(0, 3, 5)
 
@@ -37,9 +49,24 @@ object OpennedSpec
       }
     }
 
+  property("Bids that don't respect increments are always refused") =
+    forAll(posNum[Int]) { bid =>
+      val delta = bid - openned.currentPrice
+      val notBigEnoughBid = delta < getMinIncrement(openned)
+      notBigEnoughBid match {
+        case true => {
+          openned.receive(NewBid(Bid(bidder = joinedBidders.head, price = bid))) match {
+            case Answer(StatusCodes.BadRequest, _) => true
+            case _                                 => false
+          }
+        }
+        case false => true
+      }
+    }
+
   property("Bids of bidders that did not join are always refused") =
     forAll(posNum[Int]) { bidder =>
-      val bid = openned.currentPrice + openned.rule.increment.slots.head.minIncrement
+      val bid = openned.currentPrice + getMinIncrement(openned)
       val bidderJoined = openned.bidders.contains(bidder)
       bidderJoined match {
         case false => {
@@ -52,4 +79,11 @@ object OpennedSpec
       }
     }
 
+  private def getMinIncrement(openned: Openned): Price = {
+    if (openned.currentPrice <= firstSlot.endOfSlot) {
+      firstSlot.minIncrement
+    } else {
+      lastSlot.minIncrement
+    }
+  }
 }
