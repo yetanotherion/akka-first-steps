f0e032c53ef233e4649727701289a71bdc1a6a9e
Jon Alberdi <j.alberdi@criteo.com>
2018-12-16 10:31:14 +0100

    Add doc for AuctioneersService

--- src/main/scala/com/ion/trials/akka/service/ServiceHelper.scala
+++ src/main/scala/com/ion/trials/akka/service/ServiceHelper.scala
@@ -5,7 +5,8 @@ import akka.http.scaladsl.server.Directives.onComplete
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
 import akka.util.Timeout
-import com.ion.trials.akka.auction.AuctionTypes.Answer
+import com.ion.trials.akka.actors.GatherAuctionsActor.AuctionInfos
+import com.ion.trials.akka.auction.AuctionTypes.{Answer}
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -13,6 +14,9 @@ import scala.concurrent.duration._
 
 trait ServiceHelper extends JsonSupport {
   implicit lazy val timeout = Timeout(5.seconds)
+
+  def completeAuctionInfosAnswer(answer: Future[Answer[AuctionInfos]]) =
+    completeAnswer[AuctionInfos]((s, b) => complete(s, b), answer)
 
   def completeAnswer[T](onLeft: (StatusCode, T) => Route,
                         answer: Future[Answer[T]]) = {
@@ -28,4 +32,5 @@ trait ServiceHelper extends JsonSupport {
            s"Got exception ${ex.getMessage()}"))
     }
   }
+
 }
